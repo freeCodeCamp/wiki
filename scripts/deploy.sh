7af7dbcaa0b5a5b48a41f0a6094736fa3054c849
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-12-05 13:40:25 +0800

    add link prefixing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,9 +18,14 @@ node create_structure.js
 node convert_files.js
 git checkout gh-pages
 cd ..
-gatsby build
-cd public
-cp docs ../
-cp bundle.js ../
-cp index.html ../
-cp bundle.js.map ../
+gatsby build --prefix-links
+cp -r public/docs/* docs/
+cp public/bundle.js ./
+cp public/index.html ./
+cp public/bundle.js.map ./
+git add docs
+git add bundle.js.map
+git add bundle.js
+git add index.html
+git commit -m "Page built on `date +"%d-%m-%Y %T"`"
+# git push live gh-pages
