85f00aa4110334c4fc28dd377ff413fc27b4d02f
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-12-05 13:37:39 +0800

    page build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,8 +19,13 @@ node convert_files.js
 git checkout gh-pages
 cd ..
 gatsby build
-cd public
-cp docs ../
-cp bundle.js ../
-cp index.html ../
-cp bundle.js.map ../
+cp -r public/docs/* docs/
+cp public/bundle.js ./
+cp public/index.html ./
+cp public/bundle.js.map ./
+git add docs
+git add bundle.js.map
+git add bundle.js
+git add index.html
+git commit -m "Page built on `date +"%d-%m-%Y %T"`"
+git push live gh-pages
