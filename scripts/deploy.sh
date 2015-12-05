160c81e559d987a065d903e1a9ab78cc46cfebce
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-12-05 12:59:57 +0800

    Finish deploy.sh partially

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Automate Deployment -- bash script
+# - run clearPages.sh
+# - run
+# - git checkout master
+# - Pull down the Wiki
+# - Run Conversion  (See above)
+# - Run Cleanup (See above)
+# - Run Gatsby Build Operation
+# - git checkout gh-pages
+# - Copy files to `gh-pages`
+# - Commit files to that branch
+# - Git push
+
+./clearPages.sh
+node create_structure.js
+node convert_files.js
+git checkout gh-pages
+cd ..
+gatsby build
